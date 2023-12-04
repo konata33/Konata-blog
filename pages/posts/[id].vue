d12b60708466e7c00ac49c95f6f0f52bb7c50c8e
MrCat33 <1025090504@qq.com>
2023-12-04 16:29:31 +0800

    chore:update page

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,13 +1,19 @@
 <script setup>
 const route = useRoute()
+const article = await queryContent(route.params.id).findOne()
 </script>
 
 <template>
   <div h-auto w-full>
     <NavigationTabs />
     <div w-full flex flex-col items-center justify-center>
-      <div font-sans>
-        标题我的字呢？？？
+      <div
+        w-full flex justify-center py-5 text-6xl font-bold font-sans
+      >
+        {{ article.title }}
+      </div>
+      <div>
+        {{ article.date }}
       </div>
       <article prose-lg w-full flex items-center justify-center font-sans color-slate-3 prose first-letter:text-3xl>
         <ContentDoc :path="route.params.id" />
