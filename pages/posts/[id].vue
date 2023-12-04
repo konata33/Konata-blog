61a662d727476c1af61f52e3f2c5f743c0e53d5d
MrCat33 <1025090504@qq.com>
2023-12-04 15:38:48 +0800

    chore:update page

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -5,8 +5,13 @@ const route = useRoute()
 <template>
   <div h-auto w-full>
     <NavigationTabs />
-    <article prose-lg max-w-full flex items-center justify-center font-sans color-slate-3 prose first-letter:text-3xl>
-      <ContentDoc :path="route.params.id" />
-    </article>
+    <div w-full flex flex-col items-center justify-center>
+      <div font-sans>
+        标题我的字呢？？？
+      </div>
+      <article prose-lg w-full flex items-center justify-center font-sans color-slate-3 prose first-letter:text-3xl>
+        <ContentDoc :path="route.params.id" />
+      </article>
+    </div>
   </div>
 </template>
