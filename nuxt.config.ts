542ff1a432120c2459ac2d0ac239aa56aadabeac
MrCat33 <1025090504@qq.com>
2024-12-24 10:19:41 +0800

    chore:update version config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,6 @@ export default defineNuxtConfig({
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
-    inlineSSRStyles: false,
     renderJsonPayloads: true,
     typedPages: true,
   },
